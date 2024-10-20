02770ff8802fb9d94a0a3619ba98b7fb507b4d2f
Olufunke Johnston <johnstonolufunke@outlook.com>
2024-10-20 10:57:40 +0100

    make a small change

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 8000;
 
 http.createServer(function(req, res) { console.log
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Thank You World!");
+    res.end("Thank You!");
     }).listen(port, function() { 
         console.log(`Node server is running on port ${port}...`); 
 }); 
